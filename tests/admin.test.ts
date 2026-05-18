c6ed8adf4681b2ee1c76f5ccb579bf35195e0c44
Gustavo <Guuhsilva13@outlook.com>
2026-05-18 10:18:08 -0300

    testes com jest para a rota PATCH /admin/clients/:id

--- tests/admin.test.ts
+++ tests/admin.test.ts
@@ -411,4 +411,203 @@ describe("Admin", () => {
       expect(res.body).toHaveProperty("message");
     });
   });
+
+  describe("PATCH /admin/clients/:id", () => {
+    it("should update a client", async () => {
+      const { accessToken } = await getAdminToken(app);
+
+      const clientResponse = await request(app)
+        .post("/admin/clients")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          firstName: "joão",
+          lastName: "silva",
+          cpf: "94279968039",
+        });
+
+      const clientId = clientResponse.body.id;
+
+      const res = await request(app)
+        .patch(`/admin/clients/${clientId}`)
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          firstName: "marcio",
+        });
+
+      const updatedClient = res.body;
+
+      expect(res.statusCode).toBe(200);
+      expect(updatedClient.firstName).toBe("marcio");
+
+      expect(updatedClient.lastName).toBe("silva");
+      expect(updatedClient.cpf).toBe("94279968039");
+    });
+
+    it("should return 400 when route param id is not a valid UUID", async () => {
+      const { accessToken } = await getAdminToken(app);
+
+      const res = await request(app)
+        .patch("/admin/clients/invalidId")
+        .set("Authorization", `Bearer ${accessToken}`);
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toHaveProperty("errors");
+    });
+
+    it("should return 400 when firstName has less than 4 characters", async () => {
+      const { accessToken } = await getAdminToken(app);
+
+      const res = await request(app)
+        .patch("/admin/clients/ef5d81e9-f54e-453d-93e6-4116d1f70617")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          firstName: "mar",
+        });
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toHaveProperty("errors");
+    });
+
+    it("should return 400 when lastName has less than 4 characters", async () => {
+      const { accessToken } = await getAdminToken(app);
+
+      const res = await request(app)
+        .patch("/admin/clients/ef5d81e9-f54e-453d-93e6-4116d1f70617")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          lastName: "ri",
+        });
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toHaveProperty("errors");
+    });
+
+    it("should return 400 when CPF is invalid", async () => {
+      const { accessToken } = await getAdminToken(app);
+
+      const res = await request(app)
+        .patch("/admin/clients/ef5d81e9-f54e-453d-93e6-4116d1f70617")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          cpf: "00000000000",
+        });
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toHaveProperty("errors");
+    });
+
+    it("should return 400 when the required fields are missing", async () => {
+      const { accessToken } = await getAdminToken(app);
+
+      const res = await request(app)
+        .patch("/admin/clients/ef5d81e9-f54e-453d-93e6-4116d1f70617")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({});
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toHaveProperty("errors");
+    });
+
+    it("should return 404 if client does not exist", async () => {
+      const { accessToken } = await getAdminToken(app);
+
+      const res = await request(app)
+        .patch("/admin/clients/ef5d81e9-f54e-453d-93e6-4116d1f70617")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          firstName: "marcio",
+        });
+
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toHaveProperty("message");
+    });
+
+    it("should return 409 if client is inactive", async () => {
+      const { accessToken } = await getAdminToken(app);
+
+      const { id: clientId } = await prisma.client.create({
+        data: await createClient("joão", "silva", "94279968039", false),
+      });
+
+      const res = await request(app)
+        .patch(`/admin/clients/${clientId}`)
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          firstName: "marcio",
+        });
+
+      expect(res.statusCode).toBe(409);
+      expect(res.body).toHaveProperty("message");
+    });
+
+    it("should return 400 when CPF is already in use", async () => {
+      const { accessToken } = await getAdminToken(app);
+
+      const responseClient = await request(app)
+        .post("/admin/clients")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          firstName: "joão",
+          lastName: "silva",
+          cpf: "94279968039",
+        });
+
+      const clientId = responseClient.body.id;
+
+      const res = await request(app)
+        .patch(`/admin/clients/${clientId}`)
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          cpf: "94279968039",
+        });
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toHaveProperty("message");
+    });
+
+    it("should return 401 when the session has already been invalidated", async () => {
+      const { accessToken } = await getAdminToken(app);
+
+      await request(app)
+        .post("/auth/logout")
+        .set("Authorization", `Bearer ${accessToken}`);
+
+      const res = await request(app)
+        .patch("/admin/clients/ef5d81e9-f54e-453d-93e6-4116d1f70617")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          firstName: "marcio",
+        });
+
+      expect(res.statusCode).toBe(401);
+      expect(res.body).toHaveProperty("message");
+    });
+
+    it("should return 401 if accessToken is missing", async () => {
+      const res = await request(app)
+        .patch("/admin/clients/ef5d81e9-f54e-453d-93e6-4116d1f70617")
+        .send({
+          firstName: "marcio",
+        });
+
+      expect(res.statusCode).toBe(401);
+      expect(res.body).toHaveProperty("message");
+    });
+
+    it("should return 403 if accessToken is expired", async () => {
+      const expiredToken = jwt.sign({ sub: "user-id" }, env.TOKEN_SECRET, {
+        expiresIn: "-1h",
+      });
+
+      const res = await request(app)
+        .patch("/admin/clients/ef5d81e9-f54e-453d-93e6-4116d1f70617")
+        .set("Authorization", `Bearer ${expiredToken}`)
+        .send({
+          firstName: "marcio",
+        });
+
+      expect(res.statusCode).toBe(403);
+      expect(res.body).toHaveProperty("message");
+    });
+  });
 });
